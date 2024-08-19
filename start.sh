e6bc04327ce148b2ad8222b7538566b04d24b126
Matias Pereira <67346853+matiaspereira3005@users.noreply.github.com>
2024-08-19 16:50:55 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. /opt/venv/bin/activate  # Activar el entorno virtual
-flask init-db             # Inicializar la base de datos
-exec gunicorn app:create_app()  # Iniciar el servidor WSGI
