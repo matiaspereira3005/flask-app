850d2e45f0af822b42e49e9aa486aba3a32f0476
matiaspereira3005 <matiaspereira3005@gmail.com>
2024-08-21 20:46:03 -0400

    Se actualiza archivo de requerimientos

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,14 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT'),
         FROM_EMAIL=os.environ.get('FROM_EMAIL'),
         MAILJET_KEY=os.environ.get('MAILJET_KEY'),
         SECRET_KEY=os.environ.get('SECRET_KEY'),
-        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
-        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
-        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
-        DATABASE=os.environ.get('FLASK_DATABASE'),
-        DATABASE_PORT=os.environ.get('FLASK_DATABASE_PORT'),
     )
 
     from . import db
